2be000471afe3dc370102428825c71dedb8f5506
Syed Ali hasan <syedalihasan10@gmail.com>
2022-04-28 21:56:09 +0530

    improved stripe bugs

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -4,7 +4,6 @@ const Tour = require('../models/tourModel');
 const User = require('../models/userModel');
 const Booking = require('../models/bookingModel');
 const factory = require('./handlerFactory');
-const AppError = require('./../utils/AppError');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) get the currently booked tour
@@ -51,7 +50,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  price = session.display_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
 exports.webhookCheckout = (req, res, next) => {
